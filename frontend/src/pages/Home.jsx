1615d863971b2a0bd9d527571e6df0cb0eec2055
arcticzombieyt <zszombiesize@gmail.com>
2023-02-17 05:43:22 -0500

    second commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://intellipaint.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
